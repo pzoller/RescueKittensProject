03936a63149d3fee3fda7ff05ba0493ce7485689
Phil Zoller <p_zoller@yahoo.com>
2018-07-23 23:03:31 -0700

    Changed command words to enum.

---
+++ src/com/pzrider/game/CommandWord.java
@@ -0,0 +1,34 @@
+package com.pzrider.game;
+
+/**
+ * Representations for all the valid command words for the game
+ * along with a string in a particular language.
+ *
+ * @author Michael Kölling and David J. Barnes
+ * @version 2016.02.29
+ * @version 2018.07.23  Add 'look' command
+ */
+public enum CommandWord {
+    // A value for each command word along with its
+    // corresponding user interface string.
+    GO("go"), QUIT("quit"), HELP("help"), LOOK("look"),UNKNOWN("?");
+
+    // The command string.
+    private String commandString;
+
+    /**
+     * Initialise with the corresponding command string.
+     *
+     * @param commandString The command string.
+     */
+    CommandWord(String commandString) {
+        this.commandString = commandString;
+    }
+
+    /**
+     * @return The command word as a string.
+     */
+    public String toString() {
+        return commandString;
+    }
+}
